4cc187d4b66d70a22f29fda316b85b84ee3cc0be
prince <princekhant6611@gmail.com>
2024-12-14 17:14:15 +0530

    Create User Model

--- backend/prisma/migrations/20241214114131_init/migration.sql
+++ backend/prisma/migrations/20241214114131_init/migration.sql
@@ -1,19 +1,24 @@
+-- CreateEnum
+CREATE TYPE "Provider" AS ENUM ('GOOGLE', 'FACEBOOK', 'X');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
-    "name" TEXT,
-    "email" TEXT,
-    "avatar" TEXT,
-    "provider" TEXT NOT NULL,
+    "nickName" TEXT,
+    "fullName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "avatar" TEXT NOT NULL,
+    "gender" TEXT,
+    "birthday" TIMESTAMP(3),
+    "phoneNumber" TEXT,
+    "verified" BOOLEAN NOT NULL,
+    "provider" "Provider" NOT NULL,
     "providerId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_providerId_key" ON "User"("providerId");
