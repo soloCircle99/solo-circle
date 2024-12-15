d4caae13723816b38d5d50ff84f098895e8f4f12
het52004 <hetgajjar332@gmail.com>
2024-12-15 22:24:38 +0530

    added table and sidebar

---
+++ frontend/src/components/admin/Sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+    return (
+        <div className="h-full w-[350px] border border-2 border-sky-500">
+            sidebar
+        </div>
+    )
+}
+
+export default Sidebar
--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,10 +1,17 @@
 import Header from "../components/admin/Header"
+import Sidebar from "../components/admin/Sidebar"
+import Table from "../components/admin/Table"
 
 const Admin = () => {
   return (
     <div className="h-[calc(100vh-60px)] w-full">
-      <Header/>
-      This is admin
+      <Header />
+      <div className="flex">
+        <Sidebar />
+        <div>
+          <Table />
+        </div>
+      </div>
     </div>
   )
 }
